f42e7c5972c6ceabd99bbced6f95709a1c6e3ccc
erfangoodazi <63591256+erfan-goodarzi@users.noreply.github.com>
2021-08-23 20:31:32 +0430

    Add Checkbox to task component

--- src/Components/MainTask.jsx
+++ src/Components/MainTask.jsx
@@ -2,31 +2,19 @@ import React from 'react'
 import styled from 'styled-components'
 import {Task} from './Task'
 
-const Eg = styled.div`
+const Main = styled.div`
 height: 0vh;
 
 `
 
 export const MainTask = () => {
     return (
-        <Eg>
+        <Main>
             <Task id={0} />
             <Task id={1} />
             <Task id={2} />
             <Task id={3} />
-            <Task id={3} />
-            <Task id={3} />
-            <Task id={3} />
-            <Task id={3} />
-            <Task id={3} />
-            <Task id={3} />
-            <Task id={3} />
-            <Task id={3} />
-            <Task id={3} />
-            <Task id={3} />
-            <Task id={3} />
-            <Task id={3} />
-            <Task id={3} />
-        </Eg>
+    
+        </Main>
     )
 }
